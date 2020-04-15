0da8f2cd98e43b40c25bdb37be9b35877657e3be
xxx44552 <amfABxPU>
2020-04-15 17:19:18 +0300

    save

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+//window.addEventListener('load', () => chrome.storage.local.clear(), false);
+
 const range = document.getElementById('range');
 const num = document.getElementById('num');
+const bg1 = document.getElementById('bg-1');
+const bg2 = document.getElementById('bg-2');
+const mediaQuery = document.getElementById('media-query');
 
-window.addEventListener('load', function (e) {
-  chrome.storage.local.clear(function() {
-    var error = chrome.runtime.lastError;
-    if (error) {
-      console.error(error);
-    }
-  });
-}, false);
+bg2.addEventListener('input', function () {
+  bg2.value === '' ? mediaQuery.style.display = 'none' : mediaQuery.style.display = 'block';
+})
+
+chrome.storage.local.get(['key'], function(result) {
+  console.log('Value currently is ' + result.key);
+  if(result.key) {
+    num.innerHTML = result.key;
+    range.value = result.key;
+  }
+});
 
 range.addEventListener('input', function () {
   num.innerHTML = range.value;
@@ -16,10 +24,3 @@ range.addEventListener('input', function () {
     console.log('Value is set to ' + range.value);
   });
 });
-
-
-chrome.storage.local.get(['key'], function(result) {
-  console.log('Value currently is ' + result.key);
-  if(result.key) num.innerHTML = result.key
-});
-
